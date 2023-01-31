259a3107a38f518fa4c0df540b3a85409bf4344e
Dilara <dilaraismn@gmail.com>
2023-01-31 10:47:30 +0300

    Apple Random Start Position

---
+++ Assets/_Scripts/AppleScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AppleScript : MonoBehaviour
+{
+    public BoxCollider2D gameAreaCollider;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gameAreaCollider.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0);
+    }
+}
