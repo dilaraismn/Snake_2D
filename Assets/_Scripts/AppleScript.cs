07fee8abe0b543d8d83e86aaf52cfbcada068cce
Dilara <dilaraismn@gmail.com>
2023-02-01 16:01:46 +0300

    Difficulty 03

--- Assets/_Scripts/AppleScript.cs
+++ Assets/_Scripts/AppleScript.cs
@@ -9,6 +9,8 @@ public class AppleScript : MonoBehaviour
     public BoxCollider2D gameAreaCollider;
     public GameObject snake;
     private float minDistance = 5;
+    private float minAppleDistance = 5;
+    private List<GameObject> allActiveApples = new List<GameObject>();
 
     private void Start()
     {
@@ -19,11 +21,12 @@ public class AppleScript : MonoBehaviour
         
         RandomizePosition();
     }
-
+    
     public void RandomizePosition()
     {
-        while (Vector2.Distance(this.transform.position, snake.transform.position) < minDistance || 
-               IsTooCloseToTail(this.transform.position))
+        while (Vector2.Distance(this.transform.position, snake.transform.position) < minDistance 
+               || IsTooCloseToTail(this.transform.position) 
+               || IsApplesTooClose(this.transform.position))
         {
             Bounds bounds = this.gameAreaCollider.bounds;
             float x = Random.Range(bounds.min.x, bounds.max.x);
@@ -37,7 +40,6 @@ public class AppleScript : MonoBehaviour
 
     private bool IsTooCloseToTail(Vector2 applePosition)
     {
-
         foreach (Transform tailSegment in SnakeScript._segments)
         {
             if (Vector2.Distance(applePosition, tailSegment.transform.position) < minDistance)
@@ -45,6 +47,25 @@ public class AppleScript : MonoBehaviour
                 return true;
             }
         }
+        return false;
+    }
+
+    private bool IsApplesTooClose(Vector2 applePos)
+    {
+        foreach (GameObject apple in allActiveApples)
+        {
+            if (apple.activeInHierarchy)
+            {
+                allActiveApples.Add(apple);
+            }
+
+            if (Vector3.Distance(applePos, apple.transform.position) < minAppleDistance)
+            {
+                print("apples are too close");
+                return true;
+            }
+        }
+        print("okay");
         return false;
     }
     
